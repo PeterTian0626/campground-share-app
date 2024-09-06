9fc4a966c98eae4c32f78029015df1e8ac70b68b
Peter <petertian626@gmail.com>
2024-09-06 17:47:44 -0400

    updated homepage navbar and index page (removed 'new campgrounds' link)

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const store = MongoStore.create({
 })
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.set('views', path.join(__dirname, 'views'))
 // app.set('engine', ejsMate)
--- models/seeds/index.js
+++ models/seeds/index.js
@@ -3,17 +3,18 @@ const Campground = require('../campground')
 const Locations = require("./cities")
 const { descriptors, places } = require("./camps")
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+// mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+mongoose.connect("mongodb+srv://petertian626:aGI8AxmQuIXX8SwO@cluster0.pgk7z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 const sample = (arr) => arr[Math.floor(Math.random() * arr.length) + 1]
 
 const seedDB = async () => {
   await Campground.deleteMany({})
-  for (let i = 0; i < 40; i++) {
+  for (let i = 0; i < 100; i++) {
     const price = 20 + Math.floor(Math.random() * 30)
     const random = Math.round(Math.random() * 1000)
     const location = `${Locations[random].city}, ${Locations[random].state}`
-    const title = `${sample(descriptors)} ${sample(places)}`
+    const title = `${sample(descriptors)} ${sample(places)} (Seed Data)`
     const camp = new Campground({
       title: title,
       location: location,
@@ -24,7 +25,7 @@ const seedDB = async () => {
       price: price,
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas obcaecati excepturi dicta iure aut perferendis esse repellendus error sequi, soluta, similique fuga odit incidunt! Natus blanditiis vel beatae quisquam expedita!",
       image: [{ url: `https://res.cloudinary.com/dx8eansli/image/upload/v1725390083/YelpCamp/evkku5j3rqgrm9ybsg3g.jpg`, filename: 'seed_filename' }],
-      author: '66b38b0dfa50087a4c8e598f'
+      author: '66db6a303768b62873d1272f'
     })
     await camp.save()
   }
